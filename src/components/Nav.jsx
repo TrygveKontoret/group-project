ec2533ef31011e387ca87324bc0fe629121e3c99
Ole994 <69842859+Ole994@users.noreply.github.com>
2022-03-09 14:50:10 +0100

    filter in progress

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,51 +1,75 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import styled from 'styled-components'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+//span,
+// p {
+//     font-family: "Inconsolata", monospace;
+//https://fonts.google.com/specimen/Inconsolata?query=Inconsolata
+//
+
+//     color: white;
+//   }
+
+//   h1,
+//   h2,
+//   h3 {
+//     font-family: "Outfit", sans-serif;
+//     color: white;
+//   }
 
 const StyledUl = styled.ul`
-    list-style-type: none;
-    padding: 0;
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #89b5c3;
 `;
+
+//Color palette
+
+// #80421B      #C4764A  #E5AA88    #89B5C3     #326580
 
 const StyledLink = styled(Link)`
-    background-color: #b00b69;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 10px;
-    border-radius: 8px;
-    transition: 2s;
+  background-color: #326580;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 10px;
+  border-radius: 8px;
+  transition: 2s;
 
-    &:hover {
-        transform:rotate(360deg);
-        transition: 2s;
-    }
+  &:hover {
+    transform: rotate(360deg);
+    transition: 2s;
+  }
 `;
 
-const Nav = ({title}) => {
+const Nav = ({ title }) => {
   return (
     <>
-        <StyledUl>
-            <StyledLink to='/'>
-                <li>Home</li>
-            </StyledLink>
-            <StyledLink to='/contact'>
-                <li>Contact</li>
-            </StyledLink>
-            <StyledLink to='/admin'>
-                <li>Admin</li>
-            </StyledLink>
-            <StyledLink to='/login'>
-                <li>Login</li>
-            </StyledLink>
-        </StyledUl>
-        <h1>{title}</h1>
+      <StyledUl>
+        <StyledLink to="/">
+          <li>Home</li>
+        </StyledLink>
+        <StyledLink to="/contact">
+          <li>Contact</li>
+        </StyledLink>
+        <StyledLink to="/admin">
+          <li>Admin</li>
+        </StyledLink>
+        <StyledLink to="/login">
+          <li>Login</li>
+        </StyledLink>
+      </StyledUl>
+      <h1>{title}</h1>
     </>
-  )
-}
+  );
+};
 
 export default Nav;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import Nav from '../../components/Nav';
+import React from "react";
+import Nav from "../../components/Nav";
+//Searchbar
+import SearchBar from "../../components/SearchBar";
 
 const Home = () => {
   return (
     <>
-        <Nav title='Home' />
+      <Nav title="Home" />
+      <Searchbar />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
