5c973bdf6352c0fe8575acf4f45b4b027559dff8
Thomas Kildahl <69842791+ThomasK97@users.noreply.github.com>
2022-03-10 13:39:52 +0100

    feat styled form, fixed schema

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,25 +1,29 @@
-import React, { useContext, useEffect } from 'react';
-import Nav from '../../components/Nav';
-import { useForm } from 'react-hook-form';
-import { yupResolver, yupRessolver } from '@hookform/resolvers/yup';
-import { userLoginSchema } from '../../utils/yupSchemas';
-import axios from 'axios';
-import { useNavigate } from 'react-router';
-import AuthContext from '../../context/AuthContext';
-
+import React, { useContext, useEffect } from "react";
+import Nav from "../../components/Nav";
+import { useForm } from "react-hook-form";
+import { yupResolver, yupRessolver } from "@hookform/resolvers/yup";
+import { userLoginSchema } from "../../utils/yupSchemas";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import AuthContext from "../../context/AuthContext";
+import { AUTH_URL } from "../../utils/Api";
+import styled from "styled-components";
+import { StyledForm } from "./styledLogin";
+import { StyledButton } from "./styledLogin";
+import { StyledInput } from "./styledLogin";
+import { StyledSpan } from "./styledLogin";
 
 const Login = () => {
-
   const navigate = useNavigate();
 
   const [auth, setAuth] = useContext(AuthContext);
 
-  useEffect(()=> {
-    console.log(auth);
-
+  useEffect(() => {
+    console.log("auth", auth);
+    console.log(AUTH_URL);
   }, []);
 
-  const{
+  const {
     register,
     handleSubmit,
     formState: { errors },
@@ -33,42 +37,37 @@ const Login = () => {
       password: formData.password,
     });
 
-    console.log('Response Data:', responseData);
+    console.log("Response Data:", responseData);
 
     setAuth(responseData.data.jwt);
-    navigate('/admin');
+    navigate("/admin");
   };
 
   const onSubmit = (formData) => {
-    console.log('Form Data: ', formData);
+    console.log("Form Data: ", formData);
 
     loginUser(formData).catch(console.error);
   };
 
-  
   return (
-
-    
-    
     <>
-        <Nav title='Login' />
-        
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('email')} placeholder='Your email...' />
-        {errors.email && <span>{errors.email.message}</span>}
+      <Nav title="Login" />
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+        <StyledInput {...register("email")} placeholder="Your email..." />
+        {errors.email && <StyledSpan> {errors.email.message}</StyledSpan>}
 
-        <input
-          {...register('password')}
-          type='password'
-          placeholder='Your password...'
+        <StyledInput
+          {...register("password")}
+          type="password"
+          placeholder="Your password..."
         />
-        {errors.password && <span>{errors.password.message}</span>}
 
-        <button>Login</button>
-      </form>
-        
+        {errors.password && <StyledSpan>{errors.password.message}</StyledSpan>}
+
+        <StyledButton>Login</StyledButton>
+      </StyledForm>
     </>
-  )
-}
+  );
+};
 
 export default Login;
--- src/utils/yupSchemas.js
+++ src/utils/yupSchemas.js
@@ -3,8 +3,7 @@ import * as yup from 'yup';
 export const userLoginSchema = yup.object().shape({
     email: yup
     .string()
-    .required('Please enter you email adress')
-    .email('Pkease enter valid email adress'),
+    .required('Please enter you email adress'),    
     password: yup.string().required('Please enter your password'),
 });
 
